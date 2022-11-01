fa2fffb67582a7f5e3fe0d6efc5c8478faf493a7
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2022-11-01 14:59:51 +0300

    not ortalaması

--- src/notOrtalamasi.java
+++ src/notOrtalamasi.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
 
 public class notOrtalamasi {
     public static void main(String[] args) {
-        
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Matematik Notunuz: ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz: ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz: ");
+        kimya = inp.nextInt();
+
+        System.out.print("Türkçe Notunuz: ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz: ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Notunuz: ");
+        muzik = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam / 6;
+        System.out.println("Ortalamanız : " + sonuc);
+
+        boolean durum = sonuc > 60;
+        System.out.println(durum ? "Sınıfı Geçti" : "Sınıfta Kaldı");
+
     }
 }
